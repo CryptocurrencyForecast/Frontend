b855b729aa57f1f8af640ab52ef4d80d3fff185c
MTLantoine <mortelier.antoine@hotmail.fr>
2021-06-12 17:37:34 +0200

    init coin

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Coin from "../Coin/Coin";
 import axios from "axios";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -11,13 +13,43 @@ function HomePage() {
       )
       .then((res) => {
         setCoins(res.data);
-        console.log(res.data);
-      });
+      })
+      .catch((error) => console.log(error));
   });
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
-    <div className="HomePage">
-      <h1>HAHHAHAHAHAHAHAHHA</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1>Search a currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+          />
+        );
+      })}
     </div>
   );
 }
