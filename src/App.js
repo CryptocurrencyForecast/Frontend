03770b0594a83e22b47cbdfafdbc1ba1b6aeda68
MTLantoine <mortelier.antoine@hotmail.fr>
2021-06-12 21:08:33 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import "./App.css";
+import "./App.scss";
 import HomePage from "./components/HomePage/HomePage";
 import PostList from "./components/Reddit/PostList";
 
-
 function App() {
-    return (
-        <div className="App">
-            <HomePage/>
-            <PostList ticker="BTC"/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <HomePage />
+      <PostList ticker="BTC" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Coin.scss";
 
 const Coin = ({
   name,
