e7f270a5fd5054e88f206c65f4dabd9d5eca6704
Louis Dutoit <louis.dutoitld@gmail.com>
2021-06-13 21:43:43 +0200

    [WIP] portfolio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import RedditChartbar from "./components/Reddit/RedditChartbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CoinPage from "./components/CoinPage/CoinPage";
 import Page404 from "./components/404/404";
+import BalanceView from "./components/Binance/Portfolio";
+import Wallet from "./components/Binance/Wallet";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
             <RedditChartbar />
           </Route>
           <Route exact path="/crypto/:id" component={CoinPage} />
+          <Route exact path="/portfolio" component={BalanceView} />
           <Route component={Page404} />
         </Switch>
       </Router>
--- src/components/Binance/Portfolio.js
+++ src/components/Binance/Portfolio.js
@@ -2,33 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Balance from "./Balance";
 
-function BalanceView({token = "UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBMXVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3"}) {
-    
-    const [balances, setBalances] = useState([]);
-    
-    useEffect(() => {
-        axios
-            .get(
-                "http://127.0.0.1:5000/balance/" + token
-            )
-            .then((res) => {
-                setBalances(res.data);
-            })
-            .catch((error) => console.log(error));
-    },[]);
-    console.log(balances)
+function BalanceView({
+  c_token = "UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBMXVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3",
+}) {
+  const [balances, setBalances] = useState([]);
+  const [token, setToken] = useState({});
 
-    return (
-        <div className="balance-list">
-            {balances.map((balance) => {
-                return (
-                    <Balance
-                        asset={balance.asset}
-                        balance={balance.free}
-                    />)
-            })}
-        </div>
-    );
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/balance/" + token)
+      .then((res) => {
+        setBalances(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="balance-list">
+      {balances.map((balance) => {
+        return (
+          <div>
+            <div>
+              <form className="wallet-form">
+                <input
+                  type="text"
+                  placeholder="API key"
+                  className="api-key"
+                  value="UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBM"
+                />
+                <input
+                  type="text"
+                  placeholder="API secret"
+                  className="api-secret"
+                  value="XVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3"
+                />
+              </form>
+            </div>
+            <Balance asset={balance.asset} balance={balance.free} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default BalanceView;
