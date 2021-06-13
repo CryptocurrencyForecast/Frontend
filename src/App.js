44249cc8d7a164c16fb579b54e5f4ea0d4b9d89c
Logan <chenavier.logan@gmail.com>
2021-06-13 17:57:51 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.scss";
 import HomePage from "./components/HomePage/HomePage";
+import Reddit from "./components/Reddit/RedditChartbar";
 import PostList from "./components/Reddit/PostList";
 import RedditChartbar from "./components/Reddit/RedditChartbar";
 import Spinner from "./components/Spinner/Spinner";
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             {/* <PostList ticker="BTC" /> */}
-            <HomePage/>
+            <BrowserRouter>
+            <Switch>
+          <Route path="/antoine"><HomePage/></Route>
+          <Route path="/pierre"><Reddit/></Route>
+        </Switch>
+        </BrowserRouter>
+           
             {/* <Spinner/> <RedditChartbar/> */}
         </div>
     );
