cecc837fbcb020fa6e8448ee6e1593b9bcbb73c9
MTLantoine <mortelier.antoine@hotmail.fr>
2021-06-13 21:40:19 +0200

    coin page in progress

--- src/components/CoinPage/CoinPage.js
+++ src/components/CoinPage/CoinPage.js
@@ -4,39 +4,57 @@ import { Link } from "react-router-dom";
 import HomeIcon from "@material-ui/icons/Home";
 import axios from "axios";
 import "./CoinPage.scss";
+import Page404 from "../404/404";
+import Chart from "../Chart/Chart";
+import TechnicalAnalysis from "../TechnicalAnalysis/TechnicalAnalysis";
 
 const CoinPage = () => {
   let { id } = useParams();
 
-  const [crypto, setCrypto] = useState([]);
+  const [crypto, setCrypto] = useState({});
+  const [wrongPage, setWrongPage] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      axios
-        .get(
-          `https://api.nomics.com/v1/currencies?key=f40c6b7456c197028c38acdd0a2b6c23&ids=${id}`
-        )
-        .then((res) => {
-          setCrypto(res.data);
-          console.log(res.data);
-        })
-        .catch((error) => console.log(error));
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    axios
+      .get(
+        `https://api.nomics.com/v1/currencies?key=f40c6b7456c197028c38acdd0a2b6c23&ids=${id}`
+      )
+      .then((res) => {
+        if (res.data.length === 0) {
+          setWrongPage(true);
+        } else {
+          setCrypto(...res.data);
+        }
+        console.log(...res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [10000]);
 
   return (
     <div className="coin-page">
-      <div className="home-page-button">
-        <Link to="/">
-          <HomeIcon />
-        </Link>
-      </div>
-      <div className="coin-page__graph">GRAPHIQUE HERE</div>
-      <div className="coin-page__content">
-        <div className="coin-page__content__left-part">LEFT PART</div>
-        <div className="coin-page__content__right-part">RIGHT PART</div>
-      </div>
+      {wrongPage ? (
+        <Page404 />
+      ) : (
+        <div>
+          <div className="home-page-button">
+            <Link to="/">
+              <HomeIcon />
+            </Link>
+          </div>
+          <div className="coin-page__graph">
+            <Chart symbol={`${id}USDT`} />
+          </div>
+          <div className="coin-page__content">
+            <div className="coin-page__content__left-part">
+              <TechnicalAnalysis symbol={`${id}USDT`} />
+            </div>
+            <div className="coin-page__content__right-part">
+              <div className="description">{crypto.description}</div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/TechnicalAnalysis/TechnicalAnalysis.js
+++ src/components/TechnicalAnalysis/TechnicalAnalysis.js
@@ -1,16 +1,17 @@
 import React from "react";
-import TechnicalReview, { THEMES, INTERVALS } from 'react-tradingview-technical-analysis';
+import TechnicalReview, {
+  THEMES,
+  INTERVALS,
+} from "react-tradingview-technical-analysis";
 
-const TechnicalAnalysis = ({
-  symbol
-}) => {
+const TechnicalAnalysis = ({ symbol }) => {
   return (
     <TechnicalReview
-	  symbol={symbol} 
-    colorTheme ={THEMES.DARK}
-    interval={INTERVALS.ONE_WEEK}
-/>
+      symbol={symbol}
+      colorTheme={THEMES.DARK}
+      interval={INTERVALS.ONE_WEEK}
+    />
   );
 };
 
-export default TechnicalAnalysis
+export default TechnicalAnalysis;
