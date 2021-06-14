4d63d981113de8cb003ef901f5212ee4e86313f9
MTLantoine <mortelier.antoine@hotmail.fr>
2021-06-14 21:58:00 +0200

    sorry lucas tu avais raison

--- src/components/CoinPage/CoinPage.js
+++ src/components/CoinPage/CoinPage.js
@@ -19,9 +19,7 @@ const CoinPage = () => {
 
   useEffect(() => {
     axios
-      .get(
-        `https://api.nomics.com/v1/currencies?key=f40c6b7456c197028c38acdd0a2b6c23&ids=${id}`
-      )
+      .get(`https://cryptocurrencyforecast.herokuapp.com/nomics/${id}`)
       .then((res) => {
         if (res.data.length === 0) {
           setWrongPage(true);
