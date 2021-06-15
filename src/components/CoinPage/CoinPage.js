df184547be6680483c48f499ffb20fe7154d0e9b
MTLantoine <mortelier.antoine@hotmail.fr>
2021-06-15 23:05:21 +0200

    fix title page crypto + remove social network

--- src/components/CoinPage/CoinPage.js
+++ src/components/CoinPage/CoinPage.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import HomeIcon from "@material-ui/icons/Home";
 import axios from "axios";
 import "./CoinPage.scss";
 import Page404 from "../404/404";
@@ -9,9 +7,6 @@ import Chart from "../Chart/Chart";
 import PostList from "../Reddit/PostList";
 import TechnicalAnalysis from "../TechnicalAnalysis/TechnicalAnalysis";
 import { If } from "react-if";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import {AccountBalanceWallet} from "@material-ui/icons";
-import ListItemText from "@material-ui/core/ListItemText";
 
 const CoinPage = () => {
   let { id } = useParams();
@@ -38,9 +33,9 @@ const CoinPage = () => {
         <Page404 />
       ) : (
         <div>
-          <div>
-            <img src={crypto.logo} alt="message logo"/>
-            <div>{id}</div>
+          <div className="crypto-title">
+            <img className="image" src={crypto.logo} alt="message logo" />
+            <div className="title">{crypto.name}</div>
           </div>
           <div className="coin-page__graph">
             <Chart symbol={`${id}USD`} />
@@ -48,109 +43,6 @@ const CoinPage = () => {
           <div className="coin-page__content">
             <div className="coin-page__content__left-part">
               <TechnicalAnalysis symbol={`${id}USD`} />
-              <If
-                condition={
-                  crypto.urls ||
-                  crypto.urls ||
-                  crypto.urls ||
-                  crypto.urls ||
-                  crypto.urls ||
-                  crypto.urls
-                }
-              >
-                <div className="social block">
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png"
-                        alt="twitter logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Twitter
-                      </a>
-                    </div>
-                  </If>
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="https://mtlantoine.github.io/CV/assets/pic/svg/github.svg"
-                        alt="github logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Github
-                      </a>
-                    </div>
-                  </If>
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="https://www.elementaryos-fr.org/wp-content/uploads/2019/08/logo-reddit.png"
-                        alt="reddit logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Reddit
-                      </a>
-                    </div>
-                  </If>
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="https://cpgcanhelp.com/wp-content/uploads/2018/03/Technical-Writing.png"
-                        alt="technical doc logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Technical documentation
-                      </a>
-                    </div>
-                  </If>
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="http://www.cds07.fr/wp-content/uploads/sites/7/2020/03/website-logo-png.png"
-                        alt="website logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Website
-                      </a>
-                    </div>
-                  </If>
-                  <If condition={crypto.urls}>
-                    <div className="social__name">
-                      <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Google_Messages_logo.svg/2500px-Google_Messages_logo.svg.png"
-                        alt="message logo"
-                      />
-                      <a
-                        href={crypto.urls}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Message
-                      </a>
-                    </div>
-                  </If>
-                </div>
-              </If>
             </div>
             <div className="coin-page__content__right-part">
               <If condition={crypto.description}>
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import CustomizedMenus from "./Menu";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 export default function Navbar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" color={"inherit"} className="Header">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                        <img src={process.env.PUBLIC_URL + '/cryptofor.png'} alt="Logo"/>
-                    </Typography>
-                    <CustomizedMenus/>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={"inherit"} className="Header">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link to={`/`}>
+              <img src={process.env.PUBLIC_URL + "/cryptofor.png"} alt="Logo" />
+            </Link>
+          </Typography>
+          <CustomizedMenus />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
